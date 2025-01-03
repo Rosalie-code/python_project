f0323233cb04f61dda34effeb1c5008569713de8
Rosalie-code <rosalie.dechance@gmail.com>
2025-01-03 12:32:18 +0100

    latest version: delete useless file, added class AnalysisTool, Custom class Broker, Created User path to save file

--- src/python_project/extra_broker.py
+++ src/python_project/extra_broker.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import logging
 from dataclasses import dataclass
@@ -7,9 +8,11 @@ from datetime import datetime
 import os 
 import pickle
 from pybacktestchain.data_module import UNIVERSE_SEC, get_stocks_data, DataModule, Information
+from pybacktestchain.broker import Position, StopLoss, RebalanceFlag, Broker
 from pybacktestchain.utils import generate_random_name
 from pybacktestchain.blockchain import Block, Blockchain
 from numba import jit 
+
 from user_function import strategy_choice, get_initial_parameter
 
 # Setup logging
@@ -20,114 +23,9 @@ from datetime import timedelta, datetime
 # Classes
 #---------------------------------------------------------
 
-from pybacktestchain.broker import Position
 
 
-
-@dataclass
-class Broker:
-    cash: float 
-    positions: dict = None
-    transaction_log: pd.DataFrame = None
-    entry_prices: dict = None
-    verbose: bool = True
-
-    def initialize_blockchain(self, name: str):
-        # Check if the blockchain is already initialized and stored in the blockchain folder
-        # if folder blockchain does not exist, create it
-        if not os.path.exists('blockchain'):
-            os.makedirs('blockchain')
-        chains = os.listdir('blockchain')
-        ending = f'{name}.pkl'
-        if ending in chains:
-            if self.verbose:
-                logging.warning(f"Blockchain with name {name} already exists. Please use a different name.")
-            with open(f'blockchain/{name}.pkl', 'rb') as f:
-                self.blockchain = pickle.load(f)
-            return
-
-        self.blockchain = Blockchain(name)
-        # Store the blockchain
-        self.blockchain.store()
-
-        if self.verbose:
-            logging.info(f"Blockchain with name {name} initialized and stored in the blockchain folder.")
-
-    def __post_init__(self):
-        # Initialize positions as a dictionary of Position objects
-        if self.positions is None:
-            self.positions = {}
-
-        
-        # Initialize the transaction log as an empty DataFrame if none is provided
-        if self.transaction_log is None:
-            self.transaction_log = pd.DataFrame(columns=['Date', 'Action', 'Ticker', 'Quantity', 'Price', 'Cash'])
-
-        # Initialize the entry prices as a dictionary
-        if self.entry_prices is None:
-            self.entry_prices = {}
-
-    def buy(self, ticker: str, quantity: int, price: float, date: datetime):
-        """Executes a buy order for the specified ticker."""
-        total_cost = price * quantity
-        if self.cash >= total_cost:
-            self.cash -= total_cost
-            if ticker in self.positions:
-                position = self.positions[ticker]
-                new_quantity = position.quantity + quantity
-                new_entry_price = ((position.entry_price * position.quantity) + (price * quantity)) / new_quantity
-                position.quantity = new_quantity
-                position.entry_price = new_entry_price
-            else:
-
-                self.positions[ticker] = Position(ticker, quantity, price)
-            self.log_transaction(date, 'BUY', ticker, quantity, price)
-            self.entry_prices[ticker] = price
-        else:
-            if self.verbose:
-                logging.warning(f"Not enough cash to buy {quantity} shares of {ticker} at {price}. Available cash: {self.cash}")
-    
-    def sell(self, ticker: str, quantity: int, price: float, date: datetime):
-        """Executes a sell order for the specified ticker."""
-        if ticker in self.positions and self.positions[ticker].quantity >= quantity:
-            position = self.positions[ticker]
-            position.quantity -= quantity
-            self.cash += price * quantity
-
-            if position.quantity == 0:
-                del self.positions[ticker]
-                del self.entry_prices[ticker]
-            self.log_transaction(date, 'SELL', ticker, quantity, price)
-        else:
-            if self.verbose:
-                logging.warning(f"Not enough shares to sell {quantity} shares of {ticker}. Position size: {self.positions.get(ticker, 0)}")
-    
-    def log_transaction(self, date, action, ticker, quantity, price):
-        """Logs the transaction."""
-        transaction = pd.DataFrame([{
-            'Date': date,
-            'Action': action,
-            'Ticker': ticker,
-            'Quantity': quantity,
-            'Price': price,
-            'Cash': self.cash
-        }])
-
-        self.transaction_log = pd.concat([self.transaction_log, transaction], ignore_index=True)
-
-    def get_cash_balance(self):
-        return self.cash
-
-    def get_transaction_log(self):
-        return self.transaction_log
-
-    def get_portfolio_value(self, market_prices: dict):
-        """Calculates the total portfolio value based on the current market prices."""
-        portfolio_value = self.cash
-        for ticker, position in self.positions.items():
-            portfolio_value += position.quantity * market_prices[ticker]
-        return portfolio_value
-    
+class CustomBroker(Broker):   
     def execute_portfolio(self, portfolio: dict, prices: dict, date: datetime):
 
         nb_buy = 0 #modification
@@ -166,8 +64,7 @@ class Broker:
             diff_value = target_value - current_value
             quantity_to_trade = int(diff_value / price)
 
-            buy_port_value = buy_port_value + total_value #modification
-           
+            
             if quantity_to_trade > 0:
                 available_cash = self.get_cash_balance()
                 cost = quantity_to_trade * price
@@ -184,46 +81,55 @@ class Broker:
         total_value_after_execution = self.get_portfolio_value(prices) 
         return total_value_after_execution, nb_sell, nb_buy
 
-    def get_transaction_log(self):
-        """Returns the transaction log."""
-        return self.transaction_log
 
 @dataclass
-class RebalanceFlag:
-    def time_to_rebalance(self, t: datetime):
-        pass 
+class AnalysisTool:
+    def __init__(self, portfolio_values, initial_value, final_value, risk_free_rate=0.0):
+        self.portfolio_values = np.array(portfolio_values)
+        self.initial_value = initial_value
+        self.final_value = final_value
+        self.risk_free_rate = risk_free_rate
 
-# Implementation of e.g. rebalancing at the end of each month
-@dataclass
-class EndOfMonth(RebalanceFlag):
-    def time_to_rebalance(self, t: datetime):
-        # Convert to pandas Timestamp for convenience
-        pd_date = pd.Timestamp(t)
-        # Get the last business day of the month
-        last_business_day = pd_date + pd.offsets.BMonthEnd(0)
-        # Check if the given date matches the last business day
-        return pd_date == last_business_day
+    def total_performance(self):
+        return (self.final_value - self.initial_value) / self.initial_value
 
-@dataclass
-class RiskModel:
-    def trigger_stop_loss(self, t: datetime, portfolio: dict, prices: dict):
-        pass
+    def annualized_performance(self):
+        num_periods = len(self.portfolio_values) 
+        return (self.final_value / self.initial_value) ** (1 / num_periods) - 1
 
-@dataclass
-class StopLoss(RiskModel):
-    def trigger_stop_loss(self, t: datetime, portfolio: dict, prices: dict, broker: Broker, threshold: float ):
-        
-        for ticker, position in list(broker.positions.items()):
-            entry_price = broker.entry_prices[ticker]
-            current_price = prices.get(ticker)
-            if current_price is None:
-                logging.warning(f"Price for {ticker} not available on {t}")
-                continue
-            # Calculate the loss percentage
-            loss = (current_price - entry_price) / entry_price
-            if loss < -self.threshold:
-                logging.info(f"Stop loss triggered for {ticker} at {t}. Selling all shares.")
-                broker.sell(ticker, position.quantity, current_price, t)
+    def calculate_returns(self):
+        return np.diff(self.portfolio_values) / self.portfolio_values[:-1]
+    
+    def mean_returns(self):
+        returns = self.calculate_returns()
+        return np.mean(returns)
+    
+    def volatility_returns(self):
+        returns = self.calculate_returns()
+        return np.std(returns)
+
+    def maximum_drawdown(self):
+        cumulative_max = np.maximum.accumulate(self.portfolio_values)
+        drawdowns = (self.portfolio_values - cumulative_max) / cumulative_max
+        return drawdowns.min()
+
+    def sharpe_ratio(self):
+        returns = self.calculate_returns()
+        excess_returns = returns - self.risk_free_rate
+        return np.mean(excess_returns) / np.std(returns) if np.std(returns) > 0 else 0
+
+    def analyze(self):
+        """Portfolio Analysis Summary:"""
+        return {
+            "Portfolio Total Performance": self.total_performance(),
+            "Portfolio Annualized Performance": self.annualized_performance(),
+            "Mean of the Returns": self.mean_returns(),
+            "Volatility of the Returns": self.volatility_returns(),
+            "Maximum Drawdown": self.maximum_drawdown(),
+            "Sharpe Ratio": self.sharpe_ratio(),  
+        }
+
+
 @dataclass
 class Backtest:
     initial_date: datetime
@@ -234,7 +140,8 @@ class Backtest:
     time_column: str = 'Date'
     company_column: str = 'ticker'
     adj_close_column : str ='Adj Close'
-    rebalance_flag : type = EndOfMonth
+    rebalance_flag_class : type = RebalanceFlag
+    rebalance_flag: RebalanceFlag = None
     risk_model : type = StopLoss
     initial_cash: int = 1000000  # Initial cash in the portfolio
     name_blockchain: str = 'backtest'
@@ -243,16 +150,18 @@ class Backtest:
   
     def __post_init__(self):
         self.backtest_name = generate_random_name()
-        self.initial_cash, threshold = get_initial_parameter()
-
-        self.broker = Broker(cash=self.initial_cash, verbose=self.verbose)
+        self.initial_cash, self.threshold, self.initial_date,self.final_date  = get_initial_parameter()
+        self.broker = CustomBroker(cash=self.initial_cash, verbose=self.verbose)
         self.broker.initialize_blockchain(self.name_blockchain)
                 # Get initial cash and other parameters from user
+        self.risk_model_strategy = strategy_choice()  # Store the chosen strategy class for later use
+        self.information_class = self.risk_model_strategy 
+        self.rebalance_flag = self.rebalance_flag_class()
 
 
     def run_backtest(self):
 
-        strategy = strategy_choice()
+        strategy_name = strategy_choice()[1]
 
         evolution_nb_sell = []
         evolution_nb_buy = []
@@ -299,6 +208,15 @@ class Backtest:
                 evolution_nb_sell.append(nb_sell)
                 evolution_time.append(t)
 
+        initial_value = self.initial_cash
+        final_value = self.broker.get_portfolio_value(info.get_prices(self.final_date))
+        analysis_tool = AnalysisTool(evolution_portfolio_value, initial_value, final_value)
+        analysis_results = analysis_tool.analyze()
+        logging.info(f"Analysis results: {analysis_results}")
+
+        with open(f"backtests_analysis/statistics/analysis_results_{self.backtest_name}({strategy_name}).txt", 'w') as file:
+            for key, value in analysis_results.items():
+                file.write(f"{key}: {value}\n")
 
         logging.info(f"Backtest completed. Final portfolio value: {self.broker.get_portfolio_value(info.get_prices(self.final_date))}")
         df = self.broker.get_transaction_log()
@@ -308,7 +226,7 @@ class Backtest:
             os.makedirs('backtests')
 
         # save to csv, use the backtest name 
-        df.to_csv(f"backtests/{self.backtest_name}.csv")
+        df.to_csv(f"backtests_analysis/backtests/baktest_{self.backtest_name}({strategy_name}).csv")
 
         # store the backtest in the blockchain
         self.broker.blockchain.add_block(self.backtest_name, df.to_string())
@@ -318,12 +236,14 @@ class Backtest:
         plt.plot(evolution_time, evolution_portfolio_value, label="Portfolio Value", color='blue', marker='o')
         plt.xlabel('Time')
         plt.ylabel('Portfolio Value')
-        plt.title(f"Portfolio Value Evolution from {self.initial_date} to {self.final_date} with {strategy}")
+        plt.title(f"Portfolio Value Evolution from {self.initial_date} to {self.final_date} with {strategy_name}")
         plt.grid(True)
         plt.xticks(rotation=45)  
         plt.legend()
         plt.tight_layout()  
-        plt.show()  
+  
+        plt.savefig(f"backtests_analysis/graphs/Portfolio_Value_Evolution_with_baktest_{self.backtest_name}({strategy_name})", dpi=900)
+        plt.show()
 
         # Optionally, plot the number of buys and sells over time
         plt.figure(figsize=(10, 6))  
@@ -331,12 +251,11 @@ class Backtest:
         plt.plot(evolution_time, evolution_nb_sell, label="Number of Sells", color='red', marker='x')
         plt.xlabel('Time')
         plt.ylabel('Number of Trades')
-        plt.title(f"Buy and Sell Evolution from {self.initial_date} to {self.final_date} with {strategy}")
+        plt.title(f"Buy and Sell Evolution from {self.initial_date} to {self.final_date} with {strategy_name}")
         plt.grid(True)
         plt.xticks(rotation=45)
         plt.legend()
         plt.tight_layout()
+
+        plt.savefig(f"backtests_analysis/graphs/Number_Buy_Sell_with_baktest_{self.backtest_name}({strategy_name})", dpi=900)
         plt.show()
-
-        #TO BE CONTINUED: Implementation of statistics (mean, standard dev, etc) and save the recap on a path chosen by the user so that he can compare different strategy
-
